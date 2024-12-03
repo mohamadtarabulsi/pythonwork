cd93d293c2e3c10e32febb058e761c2ceba6fce3
mohamadtarabulsi <mo@mosalem.com>
2024-12-03 16:13:17 -0600

    Completed Chapter 5 exercise 5-1

---
+++ 5.1conditional_tests.py
@@ -0,0 +1,43 @@
+# Chapter 5-1 Exercise: Conditional Tests
+# Write a series of conditional tests. Print out a statement describing each test and your prediction for
+# the results of each test. 
+
+# 1st & 2nd conditional test
+car = 'camaro'
+print("Is car == 'camaro'? I predict True.")
+print(car == 'camaro')
+
+print("\nIs car == 'mazda'? I predict False.")
+print(car == 'mazada')
+
+# 3rd & 4th Conditional test
+name = 'mohamad'
+print("\nIs name == 'mohamad'? I predict True.")
+print(name == 'mohamad')
+
+print("\nIs name == 'lauren'? I predict False.")
+print(name == 'lauren')
+
+# 5th and 6th conditional test
+food = 'burger'
+print("\nIs food == 'burger'? I predict True.")
+print(food == 'burger')
+
+print("\nIs food == 'pizza'? I predict False.")
+print(food == 'pizza')
+
+# 7th and 8th conditional test
+restaurant = 'potbelly'
+print("\nIs restaurant == 'potbelly'? I predict True.")
+print(restaurant == 'potbelly')
+
+print("\nIs restaurant == 'firehouse subs'? I predict False.")
+print(restaurant == 'firehouse subs')
+
+# 9th and 10th conditional
+best_english_player = 'saka'
+print("\nDoes best_english_player == 'saka'? I predict True.")
+print(best_english_player == 'saka')
+
+print("\nDoes best_english_player == 'cole palmer'? I predict False.")
+print(best_english_player == 'cole palmer')

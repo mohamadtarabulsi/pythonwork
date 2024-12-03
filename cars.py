fc49d6197ed7aadbd265848093ad8d30afda9ce8
Mohamad Altarabulsi <mo@mosalem.com>
2024-12-03 14:55:58 -0600

    Added cars.py code-along

---
+++ cars.py
@@ -0,0 +1,10 @@
+# Code-along: Chapter 5
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+        

e038288f74687d83e3e3da3dd5160b35a7d68606
mohamadtarabulsi <mo@mosalem.com>
2025-02-13 23:45:56 -0600

    Finished 6-1 Exercise

---
+++ 6.1person.py
@@ -0,0 +1,10 @@
+# 6-1 Person: Use a dictionary to store information about a person you know.
+# Store their first name, last name, age, and the city in which they live.
+# Print each piece of information stored in the dictionary
+
+person = {'first_name': 'Mohamad', 'last_name': 'Altarabulsi', 'age': 22, 'city': 'Houston'}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])

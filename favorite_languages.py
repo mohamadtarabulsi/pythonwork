c5fc64468c9e8851ddbad33f8121a4f1bb016820
mohamadtarabulsi <salem.t@hotmail.com>
2025-02-15 00:08:27 -0600

    Added more notes & added 6-4 Glossary Exercise

--- favorite_languages.py
+++ favorite_languages.py
@@ -12,3 +12,16 @@ print(f"Mo's favorite language is {language}")
 for name, language in favorite_languages.items():
     print(f"\nName: {name}")
     print(f"Favorite Language: {language}")
+    
+friends = ['sarah', 'phil']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    
+if name in friends:
+    language = favorite_languages[name].title()
+    print(f"\n{name.title()}, I see you love {language}!")
+    
+# Looping Through All Values in a Dictionary 
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())

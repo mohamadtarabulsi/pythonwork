401313d32be26b0829b7a5520339af51778a27fa
mohamadtarabulsi <mo@mosalem.com>
2025-03-17 23:12:09 -0500

    Completed 8-2 Exercise

---
+++ 8.2favoriteBook.py
@@ -0,0 +1,8 @@
+# 8-2 Favorite Book: Write function named favorite_book() that accepts one parameter, title. 
+# The function should print a message, such as One of my favorite books is Alice in Wonderland. 
+# Call the function, making sure to include a book title as an argument in the function call.
+
+def favorite_book(title):
+    print(f"My favorite book is the {title}.")
+    
+favorite_book('The Man in the High Castle')
